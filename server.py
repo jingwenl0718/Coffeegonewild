3fa49da10a11817a76dba8385cfe554d299a6975
Jingwen Li <lijingwenot@gmail.com>
2022-10-16 14:21:53 -0700

    editted recipes_list.html and port

--- server.py
+++ server.py
@@ -3,4 +3,4 @@ from flask_app.controllers import users_controller, recipes_controller, comments
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5000)
